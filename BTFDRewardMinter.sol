2de7ef418d4373466f6e73acaf2c0534fbc462fa
btfdhodler <btfdhodler>
2023-07-14 07:11:23 -0500

    added onlyOwner to BTFDRewardMinter

--- BTFDRewardMinter.sol
+++ BTFDRewardMinter.sol
@@ -2,22 +2,23 @@
 
 pragma solidity 0.8.19;
 
+import "./openzeppelin/contracts/access/Ownable.sol";
 import "./IRewardMinter.sol";
 import "./BTFDToken.sol";
 
-contract BTFDRewardMinter is IRewardMinter {
+contract BTFDRewardMinter is IRewardMinter, Ownable {
   BTFDToken public token;
 
   constructor(BTFDToken _token) {
     token = _token;
   }
 
-  function safeMint(uint256 _amount) override external {
+  function safeMint(uint256 _amount) override external onlyOwner {
     try token.mint(address(this), _amount) {}
     catch {}
   }
 
-  function safeRewardTransfer(address _to, uint256 _amount) override external {
+  function safeRewardTransfer(address _to, uint256 _amount) override external onlyOwner {
     if (_amount > 0) {
       uint256 balance = token.balanceOf(address(this));
       if (_amount > balance) {
